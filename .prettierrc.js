4b0572d541e90317a1d11d438af03a525c28be8d
ikozul00 <ivana.kozul5@gmail.com>
2022-12-20 21:18:55 +0100

    initial app

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  arrowParens: 'always',
+  bracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+};
---
+++ src/features/main/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+//starting point of application, start writing code here
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Attendance record</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
