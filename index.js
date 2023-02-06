ea4991c81b4b816385e375e0872d6fd593e3f2e8
ikozul00 <ivana.kozul5@gmail.com>
2023-02-06 18:31:08 +0100

    Change message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Create GET request
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send({ message: "Express on Vercel" });
 });
 
 // Initialize server
