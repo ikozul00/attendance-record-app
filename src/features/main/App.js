160ff4f33a1810fff74163f213e40d181be5ec1b
ikozul00 <ivana.kozul5@gmail.com>
2023-02-06 18:58:58 +0100

    Add call to server

--- src/features/main/App.js
+++ src/features/main/App.js
@@ -9,12 +9,20 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
+const fetchData = async () => {
+  const data = await fetch('https://attendance-record-api.vercel.app/');
+  const jsonData = await data.json();
+  console.log(jsonData.message);
+};
+
+fetchData();
+
 //starting point of application, start writing code here
 export const App = () => {
   return (
     <SafeAreaView>
       <View>
-        <Text>Attendance record</Text>
+        <Text>Attendance record app</Text>
       </View>
     </SafeAreaView>
   );
