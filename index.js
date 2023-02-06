733a3298e476f161323e8ff308bde24f8088ba23
ikozul00 <ivana.kozul5@gmail.com>
2023-02-06 19:33:39 +0100

    Add database

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-// Add Express
-const express = require("express");
+import express from "express";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2Squim3XPlaqIlbV4lNb-Nx6Qz5JjNNQ",
+  authDomain: "attendance-record-app-7e8e5.firebaseapp.com",
+  projectId: "attendance-record-app-7e8e5",
+  storageBucket: "attendance-record-app-7e8e5.appspot.com",
+  messagingSenderId: "235122994474",
+  appId: "1:235122994474:web:2ab70ea249ca23ba9a8253",
+  databaseURL:
+    "https://attendance-record-app-7e8e5-default-rtdb.europe-west1.firebasedatabase.app",
+};
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
+//get Database
+const database = getDatabase(appFirebase);
 
 // Initialize Express
 const app = express();
 
 // Create GET request
 app.get("/", (req, res) => {
-  res.send({ message: "Express on Vercel" });
+  //read data from database (https://firebase.google.com/docs/database/web/read-and-write#web-version-9)
+  onValue(
+    ref(database, "/user/"),
+    (snapshot) => {
+      res.send({ message: snapshot.val() });
+    },
+    {
+      onlyOnce: true,
+    }
+  );
 });
 
 // Initialize server
@@ -15,4 +42,4 @@ app.listen(5000, () => {
 });
 
 // Export the Express API
-module.exports = app;
+export default app;
